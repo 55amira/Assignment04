57c4278e356d91710f1593a44737f412db76daca
amiraabdelazez <amiraabdelazez2@gmail.com>
2024-12-19 22:36:30 +0200

    One D Array

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -4,7 +4,46 @@
     {
         static void Main(string[] args)
         {
-            
+            #region One D Array
+            //int[] Numbers;
+            //Numbers = new int[5];
+            //Numbers[0] = 12;
+            //Numbers[1] = 6;
+            //Numbers[2] = 16;
+            //Numbers[3] = 18;
+            //Numbers[4] = 19;
+            //// Numbers[5] = 20; Out Of Range
+
+            //Console.WriteLine(Numbers[0]);
+            //Console.WriteLine(Numbers[1]);
+            //Console.WriteLine(Numbers[2]);
+            //Console.WriteLine(Numbers[3]);
+            //Console.WriteLine(Numbers[4]);
+
+
+            //int[] Numbers01 = { 1, 2, 2, 4, 8, 4 };
+            //int[] Numbers02 = new int[5] { 1, 2, 3, 4, 5 };
+            //int[] Numbers03 = new int[] { 5, 6, 3, 8, 8, 7 };
+
+            //Console.Write("Enter The Nunber[0] ");
+            //int.TryParse(Console.ReadLine(), out Numbers[0]);
+
+            //for(int i =0;i<Numbers.Length;i++)
+            //{
+            //    Console.Write($"Enter The Numbers[{i}] ");
+            //    int.TryParse(Console.ReadLine(), out Numbers[i]);
+
+            //}
+            //Console.WriteLine("****************");
+            //for (int i = 0; i < Numbers.Length; i++)
+            //{
+            //    Console.Write($"{Numbers[i]} "  );
+            //}
+
+            //Console.WriteLine(Numbers.Length);
+            //Console.WriteLine(Numbers.Rank); 
+            #endregion
+
         }
     }
 }
