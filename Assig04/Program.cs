18faae84d212d1ad2700da69359ad5ac8a0437ee
amiraabdelazez <amiraabdelazez2@gmail.com>
2024-12-20 03:42:41 +0200

    Q4

--- Assig04/Program.cs
+++ Assig04/Program.cs
@@ -2,51 +2,23 @@
 {
     internal class Program
     {
-        //static void SAWP(int Number01, int Number02)
-        //{
-        //    int Temp;
-        //    Temp = Number01;
-        //    Number01 = Number02;
-        //    Number02 = Temp;
-        //}
+        
 
-        static void SAWP(ref int Number01,ref int Number02)
-        {
-            int Temp;
-            Temp = Number01;
-            Number01 = Number02;
-            Number02 = Temp;
-        }
-        //static int SumArray(int[] Arr)
-        //{
-        //    int Sum = 0;
-        //    Arr[0] = 100;
-        //    for (int i = 0; i < Arr.Length; i++)
-        //    {
-        //        Sum += Arr[i];
-        //    }
-        //    return Sum;
-        //}
-        static int SumArray(ref int[] Arr)
-        {
-            int Sum = 0;
-            Arr=new int[] {4,5,6};
-            for (int i = 0; i < Arr.Length; i++)
-            {
-                Sum += Arr[i];
-            }
-            return Sum;
-        }
-        static void SumSub(int Number01, int Number02, out int Sum, out int Sub)
-        {
-            Sum = Number01 + Number02;
-            Sub = Number01 - Number02;
-        }
+        
         static void Main(string[] args)
         {
             #region Explain the difference between passing (Value type parameters) by value and by reference then write a suitable c# example.
 
             //Passing By value
+
+            //static void SAWP(int Number01, int Number02)
+            //{
+            //    int Temp;
+            //    Temp = Number01;
+            //    Number01 = Number02;
+            //    Number02 = Temp;
+            //}
+
             //int A = 5, B = 4;
             //Console.WriteLine($"A : {A}");
             //Console.WriteLine($"B : {B}");
@@ -56,6 +28,14 @@
             //Console.WriteLine($"B : {B}");
 
             ////Passing By Reference
+
+            //static void SAWP(ref int Number01, ref int Number02)
+            //{
+            //    int Temp;
+            //    Temp = Number01;
+            //    Number01 = Number02;
+            //    Number02 = Temp;
+            //}
             //Console.WriteLine($"A : {A}");
             //Console.WriteLine($"B : {B}");
             //Console.WriteLine("-------------------");
@@ -67,12 +47,34 @@
             #region Explain the difference between passing (Reference type parameters) by value and by reference then write a suitable c# example.
 
             //passing By value
+
+            //static int SumArray(int[] Arr)
+            //{
+            //    int Sum = 0;
+            //    Arr[0] = 100;
+            //    for (int i = 0; i < Arr.Length; i++)
+            //    {
+            //        Sum += Arr[i];
+            //    }
+            //    return Sum;
+            //}
+
             //int[] Numbers = { 1, 2, 3 };
             //int Sum = SumArray(Numbers);    
             //Console.WriteLine(Sum);
             //Console.WriteLine(Numbers[0]);
 
             //Passing By Reference
+            //static int SumArray(ref int[] Arr)
+            //{
+            //    int Sum = 0;
+            //    Arr = new int[] { 4, 5, 6 };
+            //    for (int i = 0; i < Arr.Length; i++)
+            //    {
+            //        Sum += Arr[i];
+            //    }
+            //    return Sum;
+            //}
             //int[] Numbers = { 1, 2, 3 };
             //int Sum = SumArray(ref Numbers);
             //Console.WriteLine(Sum);
@@ -80,6 +82,12 @@
             #endregion
 
             #region Write a c# Function that accept 4 parameters from user and return result of summation and subtracting of two numbers
+
+            //static void SumSub(int Number01, int Number02, out int Sum, out int Sub)
+            //{
+            //    Sum = Number01 + Number02;
+            //    Sub = Number01 - Number02;
+            //}
 
             //Console.WriteLine("Enter Number 1 And Number 2 ");
             //int.TryParse(Console.ReadLine(),out int Number01);
@@ -90,6 +98,30 @@
             //Console.WriteLine($"Sum = {Sum} ");
             //Console.WriteLine($"Sub = {Sub} ");
             #endregion
+
+            #region Write a program in C# Sharp to create a function to calculate the sum of the individual digits of a given number
+            //static int SumDigits(int Number)
+            //{
+            //    string Digits = Number.ToString();
+            //    int Sum = 0;
+            //    for (int i = 0; i < Digits.Length; i++)
+            //    {
+            //        Sum += Convert.ToInt32(Digits.Substring(i, 1));
+            //    }
+            //    return Sum;
+            //}
+            //  Console.WriteLine(SumDigits(55));
+            #endregion
+
+
+
+
+
+
+
+
+
+
 
         }
     }
