79ff18d21b784d652231f0a61233585f3c9affe9
amiraabdelazez <amiraabdelazez2@gmail.com>
2024-12-20 03:14:44 +0200

    Q2

--- Assig04/Program.cs
+++ Assig04/Program.cs
@@ -17,6 +17,26 @@
             Number01 = Number02;
             Number02 = Temp;
         }
+        //static int SumArray(int[] Arr)
+        //{
+        //    int Sum = 0;
+        //    Arr[0] = 100;
+        //    for (int i = 0; i < Arr.Length; i++)
+        //    {
+        //        Sum += Arr[i];
+        //    }
+        //    return Sum;
+        //}
+        static int SumArray(ref int[] Arr)
+        {
+            int Sum = 0;
+            Arr=new int[] {4,5,6};
+            for (int i = 0; i < Arr.Length; i++)
+            {
+                Sum += Arr[i];
+            }
+            return Sum;
+        }
         static void Main(string[] args)
         {
             #region Explain the difference between passing (Value type parameters) by value and by reference then write a suitable c# example.
@@ -37,6 +57,21 @@
             //SAWP(ref A,ref B);
             //Console.WriteLine($"A : {A}");
             //Console.WriteLine($"B : {B}");
+            #endregion
+
+            #region Explain the difference between passing (Reference type parameters) by value and by reference then write a suitable c# example.
+
+            //passing By value
+            //int[] Numbers = { 1, 2, 3 };
+            //int Sum = SumArray(Numbers);    
+            //Console.WriteLine(Sum);
+            //Console.WriteLine(Numbers[0]);
+
+            //Passing By Reference
+            //int[] Numbers = { 1, 2, 3 };
+            //int Sum = SumArray(ref Numbers);
+            //Console.WriteLine(Sum);
+            //Console.WriteLine(Numbers[0]);
             #endregion
         }
     }
